896254cc25846f2d47d9b7939e204d98ce18cf1a
meeplabsdev <meeplabsdev@noreply.users.github.com>
2025-06-22 12:05:55 +0100

    doc: Bump version

--- common/src/main/java/com/croaklib/CroakLibMod.java
+++ common/src/main/java/com/croaklib/CroakLibMod.java
@@ -3,13 +3,18 @@ package com.croaklib;
 import com.croaklib.client.CroakLibModClient;
 import dev.architectury.platform.Platform;
 import dev.architectury.utils.Env;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public final class CroakLibMod {
     public static final String MOD_ID = "croaklib";
+    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
     public static void init() {
         if (Platform.getEnv() == Env.CLIENT.toPlatform()) {
             CroakLibModClient.init();
         }
+
+//			LOGGER.info("Current version: {}", Platform.getMod(MOD_ID).getVersion());
     }
 }
